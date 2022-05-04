77d2dfbb708676831eca5b59bcf046e519cf3ba8
Dheeraj <sahdheeraj518@gmail.com>
2022-05-04 12:48:38 +0530

    Updated

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,10 +1,14 @@
 import React from 'react'
 // import { Post } from './Post'
 
-const PaginationButtonsList = ({Page, handleClick }) => {
+const PaginationButtonsList = ({ Page, handleClick }) => {
     return (
         <div className="col-12 d-flex justify-content-center pagination-buttons-list">
-            <button id={`button-${Page}`} onClick={handleClick} className="  btn btn-primary">Next</button>
+            {     Page===1?
+                <button id={`button-${Page}`} onClick={handleClick} className="active-btn  btn btn-primary">Next</button>
+
+            :      <button id={`button-${Page}`} onClick={handleClick} className="  btn btn-primary">Next</button>
+        }
         </div>
     )
 }
