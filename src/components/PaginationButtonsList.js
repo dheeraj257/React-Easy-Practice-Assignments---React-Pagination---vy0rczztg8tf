462cc482c358ccb8fa85775ba3ad4a69d65d76d1
Dheeraj <sahdheeraj518@gmail.com>
2022-05-06 13:09:33 +0530

    Updated

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -2,20 +2,15 @@ import React from 'react'
 import { useState } from 'react/cjs/react.production.min';
 // import { Post } from './Post'
 
-const PaginationButtonsList = ({handleClick}) => {
-    const classa=()=>{
-
-    }
-
+const PaginationButtonsList = ({Page,handleClick}) => {
 
     const array1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
         return (
             <div className="col-12 d-flex justify-content-center pagination-buttons-list">
                 {
                     array1.map((curr)=>{
-                         return   <button key={curr} onChange={classa}  id={`button-${curr}`} onClick={()=>handleClick(curr)} className={`btn ${curr===1?'active-btn':''}`}>{curr}</button>
+                         return   <button key={curr} id={`button-${curr}`} onClick={()=>handleClick(curr)} className={`btn ${curr==Page?'active-btn':''}`}>{curr}</button>
                     })
-                    
                     
                 }
             </div>
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,27 +8,24 @@ const Loader = () => <div className='col-lg-12 justify-center' id="loader">Loadi
 const PostList = () => {
   const [Data, setData] = useState([])
   const [loading, setloading] = useState(true)
-  const [Page, setPage] = useState(0);
- 
+  const [Page, setPage] = useState(1);
+
   const getData = async () => {
     const rawData = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${Page}&_limit=5`)
     return rawData.json()
   }
 
-  const pageChanger = (Page) => {  
-    document.getElementById(`button-${Page-1}`).classList.remove('active-btn')
-    document.getElementById(`button-${Page}`).classList.add('active-btn')
-      setPage(Page);
-      setloading(true)
-    }
+  const pageChanger = (Page) => {
+    setPage(Page);
+    setloading(true);
+  }
 
   useEffect(() => {
-
     getData().then((data) => {
       setData(data)
       setloading(false);
 
-    },[])
+    }, [])
 
   }, [Data])
 
@@ -43,13 +40,13 @@ const PostList = () => {
             )
           }
           )
-          
+
       }
-      <PaginationButtonsList handleClick={pageChanger} />
+      <PaginationButtonsList Page={Page} handleClick={pageChanger} />
     </div>
-    
+
   )
-    }
+}
 
 
 export { PostList };
